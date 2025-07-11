880a0d891f93ebe4297d3084b59d48e3ea967a09
Matt Stanek <mstanek4234@gmail.com>
2025-07-11 10:16:44 -0400

    Update SevenDisplay.py
    Inspiration struck last night, I can do a nested loop to clean the code up and only create one local variable instead of five

--- SevenDisplay.py
+++ SevenDisplay.py
@@ -17,24 +17,48 @@ digits = [zero, one, two, three, four, five, six, seven, eight, nine]
 display = []
 
 def seven_display(display):
-    line1 = ''
-    line2 = ''
-    line3 = ''
-    line4 = ''
-    line5 = ''
+    # Original function, technically more efficient
     
-    for number in display:
-        line1 = line1 + ' ' + number[1]
-        line2 = line2 + ' ' + number[2]
-        line3 = line3 + ' ' + number[3]
-        line4 = line4 + ' ' + number[4]
-        line5 = line5 + ' ' + number[5]
+    # Generates five local variables to hold the individual line outputs
+    # line1 = ''
+    # line2 = ''
+    # line3 = ''
+    # line4 = ''
+    # line5 = ''
 
-    print(line1) 
-    print(line2)
-    print(line3)
-    print(line4)
-    print(line5)
+    # Loop through the numbers to display and appends the dictionary of the line
+    # into the appropriate line
+    # for number in display:
+    #     line1 = line1 + ' ' + number[1]
+    #     line2 = line2 + ' ' + number[2]
+    #     line3 = line3 + ' ' + number[3]
+    #     line4 = line4 + ' ' + number[4]
+    #     line5 = line5 + ' ' + number[5]
+
+    # Print the lines individually
+    # print(line1) 
+    # print(line2)
+    # print(line3)
+    # print(line4)
+    # print(line5)
+
+    # Second attempt to make a cleaner, more concise code
+    # Technically less efficient because it is a nested loop
+
+    # Create a single variable to hold the output
+    output = ''
+
+    # Loop through each line to display
+    for line in range(1, len((zero.keys())) + 1):
+        # For each line, loop through the numbers in the list to display
+        for number in display:
+            # Append the output of the number at the specified line to the output variable
+            output += number[line] + ' '
+        # Once each number to display has been looped through, move to a new line
+        output += '\n'
+
+    # Display the output
+    print(output)
 
 def start_display():
     number = input('Please enter a number to display: ')
